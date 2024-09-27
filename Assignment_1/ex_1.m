fd4cf17a5279a3fd555a9decff80dcaac279cf47
Tyler Ardito <tylera1210@comcast.net>
2024-09-27 14:23:24 +0000

    added description on how to use git

---
+++ Assignment_1/ex_1.m
@@ -0,0 +1,24 @@
+% Author Name: Tyler Ardito
+% Email: ardito86@students.rowan.edu
+% Course: MATLAB Programming - Fall 2024
+% Assignment: Homework 1
+% Date: [Submission Date]
+
+%variables
+r = 5 %radius
+%calculate area
+a = %area
+%calculate circumference
+c = %circumference
+%printing results
+fprintf ('radius of the circle is %f \n', r)
+fprintf ('area is %f \n', a)
+fprintf('circumference is %f \n', c)
+
+area = pi*radius^2;
+
+diameter = 2*radius;
+
+circumference = pi*diameter;
+
+
